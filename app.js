14044121ab94d4066d427aa089d5f4ba09992902
luis barri <luisbarriw@gmail.com>
2023-02-15 17:35:20 +0100

    Enabled cors light

--- app.js
+++ app.js
@@ -9,12 +9,15 @@ var mobsRouter = require('./routes/mobs');
 
 var app = express();
 
+//Middleware
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//Routers
 app.use('/', indexRouter);
 app.use('/members', membersRouter);
 app.use('/mobs', mobsRouter);
